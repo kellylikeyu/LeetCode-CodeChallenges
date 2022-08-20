05b363eeb61727dafeeac07407a0054367dd330a
Kelly Li <100189509+kellylikeyu@users.noreply.github.com>
2022-08-19 22:12:03 -0700

    Create 146 LRU cache.js

---
+++ 146 LRU cache.js
@@ -0,0 +1,53 @@
+// Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+// Implement the LRUCache class: LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+//                               int get(int key) Return the value of the key if the key exists, otherwise return -1.
+//                               void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache.
+//                               If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+// Input
+// ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+// [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+// Output:  [null, null, null, 1, null, -1, null, -1, 3, 4]
+// Explanation
+// LRUCache lRUCache = new LRUCache(2);
+// lRUCache.put(1, 1); // cache is {1=1}
+// lRUCache.put(2, 2); // cache is {1=1, 2=2}
+// lRUCache.get(1);    // return 1
+// lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+// lRUCache.get(2);    // returns -1 (not found)
+// lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+// lRUCache.get(1);    // return -1 (not found)
+// lRUCache.get(3);    // return 3
+// lRUCache.get(4);    // return 4
+
+var LRUCache = function(capacity) {
+        this.cache = new Map();
+        this.capacity = capacity;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (!this.cache.has(key)) return -1;
+    const v = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, v);
+    return this.cache.get(key);
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if (this.cache.has(key)) {
+      this.cache.delete(key);
+    }
+    this.cache.set(key, value);
+    if (this.cache.size > this.capacity) {
+      this.cache.delete(this.cache.keys().next().value);  // keys().next().value returns first item's key
+    }
+  };
+
